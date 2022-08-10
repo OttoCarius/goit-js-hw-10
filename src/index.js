4d8e33820c3618b202f4061d52cd9cfa917a2eb0
OttoCarius <ottocarius1993@gmail.com>
2022-08-10 20:18:33 +0300

    start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './css/styles.css';
+const debounce = require('lodash.debounce');
+import fetchCountries from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
